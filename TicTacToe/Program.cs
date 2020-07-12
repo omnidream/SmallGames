181da7f73c72493546432dc6133440df246a39ce
omnidream <48985006+omnidream@users.noreply.github.com>
2020-07-12 15:48:07 +0200

    Started NewGame

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,7 +6,49 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrintLogo();
+            PrintMenu();
         }
+
+        public static void PrintLogo()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("████████ ██  ██████ ████████  █████   ██████ ████████  ██████  ███████ ");
+            Console.WriteLine("   ██    ██ ██         ██    ██   ██ ██         ██    ██    ██ ██      ");
+            Console.WriteLine("   ██    ██ ██         ██    ███████ ██         ██    ██    ██ █████   ");
+            Console.WriteLine("   ██    ██ ██         ██    ██   ██ ██         ██    ██    ██ ██      ");
+            Console.WriteLine("   ██    ██  ██████    ██    ██   ██  ██████    ██     ██████  ███████ ");
+            Console.WriteLine("");
+        }
+        public static void PrintMenu()
+        {
+            Console.WriteLine("Spela Luffarschak mot en vän eller mot en mycket sofistikerad Ai ;-)");
+            Console.WriteLine("Vill du spela mot någon eller mot datorn?");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("1. Spela mot datorn.");
+            Console.WriteLine("2. Spela med en kompis.");
+            Console.WriteLine("Välj alternativ 1 eller alternativ 2.");
+            String userPlayMode = Console.ReadLine();
+            if (ValidateIsNumber(userPlayMode))
+                StartNewGame();
+        }
+
+        public static bool ValidateIsNumber(String userPlayMode)
+        {
+            if(Char.IsNumber(userPlayMode, 0) && userPlayMode.Length == 1)
+                return true;
+            else
+            {
+                Console.WriteLine("Felaktiv inmatning, försök igen.");
+                PrintMenu();
+                return false;
+            }
+        }
+
+        public static void StartNewGame()
+        {
+
+        }
+        
     }
 }
